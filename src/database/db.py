5ec7ff0892215fcd69e421bc9860c6fff2d1cbcd
JuanSe251097 <88353009+JuanSe251097@users.noreply.github.com>
2024-11-18 10:40:23 -0500

    Update db.py

--- src/database/db.py
+++ src/database/db.py
@@ -1,1 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+def create_db():
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dna_records.db'
+  db = SQLAlchemy(app)
+  
+  # Database Model
+  class DNARecord(db.Model):
+      id = db.Column(db.Integer, primary_key=True)
+      sequence = db.Column(db.String, unique=True, nullable=False)
+      is_mutant = db.Column(db.Boolean, nullable=False)
+  
+  # Create the database
+  with app.app_context():
+      db.create_all()
 
